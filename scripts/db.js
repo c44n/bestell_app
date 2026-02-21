07f8ad9270f8a29b563308fbdd4588b4af61d151
Can <cantpts@outlook.de>
2026-02-21 17:26:20 +0100

    feat: json structur for meals

---
+++ scripts/db.js
@@ -0,0 +1,36 @@
+let meals = [
+    {
+        "categories": [
+            {
+                "burger_sandwiches": [
+                    {
+                        "image": "./images/",
+                        "name": "Veggie mushroom black burger",
+                        "desc": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+                        "price": "16.90",
+                    },
+                    {
+                        "image": "./images/",
+                        "name": "All meat burger",
+                        "desc": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+                        "price": "15.90",
+                    }
+                ]
+            },
+            {
+                "pizza": [
+                    {
+
+                    }
+                ]
+            },
+            {
+                "salad": [
+                    {
+
+                    }
+                ]
+            },
+        ]
+    }
+]
