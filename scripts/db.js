2a86382895346eb14f4398c2c12639008619534d
Can <cantpts@outlook.de>
2026-02-21 17:32:55 +0100

    feat: update meals json, create script.js file

--- scripts/db.js
+++ scripts/db.js
@@ -2,15 +2,16 @@ let meals = [
     {
         "categories": [
             {
+                "title": "Burger & Sandwiches",
                 "burger_sandwiches": [
                     {
-                        "image": "./images/",
+                        "image": "./images/allmeat_burger.jpg",
                         "name": "Veggie mushroom black burger",
                         "desc": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
                         "price": "16.90",
                     },
                     {
-                        "image": "./images/",
+                        "image": "./images/allmeat_burger.jpg",
                         "name": "All meat burger",
                         "desc": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
                         "price": "15.90",
@@ -18,16 +19,36 @@ let meals = [
                 ]
             },
             {
+                "title": "Pizza (30 cm)",
                 "pizza": [
                     {
-
+                        "image": "./images/allmeat_burger.jpg",
+                        "name": "Pizza Margherita",
+                        "desc": "Tomato Sauce, Mozzarella",
+                        "price": "11.90",
+                    },
+                    {
+                        "image": "./images/allmeat_burger.jpg",
+                        "name": "Pizza Chorizo",
+                        "desc": "Tomato slices, Mozzarella, Chorizo",
+                        "price": "13.90",
                     }
                 ]
             },
             {
+                "title": "Salad",
                 "salad": [
                     {
-
+                        "image": "./images/allmeat_burger.jpg",
+                        "name": "Warm beef arugula salad",
+                        "desc": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+                        "price": "16.90",
+                    },
+                    {
+                        "image": "./images/allmeat_burger.jpg",
+                        "name": "Mini green Salad",
+                        "desc": "Green salad, Cucumber, Carrots, Parsley, Radishes ",
+                        "price": "7.90",
                     }
                 ]
             },
